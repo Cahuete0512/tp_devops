57199ad336343a2b6887552fb8d44a44458b69fc
Cahuete0512 <magalie.contant@epsi.fr>
2023-07-07 14:17:24 +0200

    change firstname

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -5,7 +5,7 @@ var User = function (name) {
 describe("Checking if the user is created correctly", function () {
     it("should create the user with the correct name", function () {
         debugger
-        var tom = new User("tom");
-        tom.name.should.be.equal("tom");
+        var mag = new User("mag");
+        mag.name.should.be.equal("mag");
     });
 });
